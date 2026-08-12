0b660248f33802052e662da7c9fa713b892cd3fe
stu854 <vridhichowdhry@gmail.com>
2026-08-12 22:26:39 +0530

    Update quiz4.js

--- quizzes/quiz4.js
+++ quizzes/quiz4.js
@@ -2,11 +2,11 @@ const quiz4 = [
   // Page 1 (Q1 to Q3)
   {
     id: 1,
-    q: "'इ' अक्षर के बाद आता है:",
+    q: "'ई' अक्षर के बाद आता है:",
     img: null,
     opts: [
-      { text: "ई", img: null, correct: true },
-      { text: "उ", img: null, correct: false }
+      { text: "इ", img: null, correct: false },
+      { text: "उ", img: null, correct: true }
     ]
   },
   {
@@ -14,8 +14,8 @@ const quiz4 = [
     q: "'ओ' अक्षर वाले चित्र को चुनिए:",
     img: null,
     opts: [
-      { text: "ओखली", img: "images/p4_q2_mortar.png", correct: true },
-      { text: "औरत", img: "images/p4_q2_woman.png", correct: false }
+      { text: "ओखली", img: "images/okhli.jpeg", correct: true },
+      { text: "अनार", img: "images/pomegranate.jpeg", correct: false }
     ]
   },
   {
@@ -34,8 +34,8 @@ const quiz4 = [
     q: "उचित मिलान को चुनिए:",
     img: null,
     opts: [
-      { text: "इमली - आ", img: "images/p4_q4_tamarind_aa.png", correct: false },
-      { text: "इमली - इ", img: "images/p4_q4_tamarind_i.png", correct: true }
+      { text: "आम - आ", img: "images/mango.jpeg", correct: false },
+      { text: "इमली - ई", img: "images/tamarind.jpeg", correct: true }
     ]
   },
   {
@@ -43,25 +43,25 @@ const quiz4 = [
     q: "'इ' अक्षर वाले चित्र को पहचानिए:",
     img: null,
     opts: [
-      { text: "इमली", img: "images/p4_q5_tamarind.png", correct: true },
-      { text: "अमरूद", img: "images/p4_q5_guava.png", correct: false }
+      { text: "इमली", img: "images/tamarind.jpeg", correct: true },
+      { text: "अमरूद", img: "images/guava.jpeg", correct: false }
     ]
   },
 
   // Page 3 (Q6 to Q8)
   {
     id: 6,
-    q: "'आ' अक्षर वाले चित्र को चुनिए:",
+    q: "'ए' अक्षर वाले चित्र को चुनिए:",
     img: null,
     opts: [
-      { text: "आम", img: "images/p4_q6_mango.png", correct: true },
-      { text: "अनार", img: "images/p4_q6_pomegranate.png", correct: false }
+      { text: "एकतारा", img: "images/ektara.jpeg", correct: true },
+      { text: "तलवार", img: "images/sword.jpeg", correct: false }
     ]
   },
   {
     id: 7,
     q: "दिए गए अक्षर से अलग अक्षर को पहचानिए:",
-    img: null,
+    img: "images/aa_hindi_letter.jpeg",
     opts: [
       { text: "इ", img: null, correct: true },
       { text: "आ", img: null, correct: false }
@@ -89,11 +89,11 @@ const quiz4 = [
   },
   {
     id: 10,
-    q: "'ऋ' अक्षर के बाद आता है:",
+    q: "'अ' अक्षर के बाद आता है:",
     img: null,
     opts: [
       { text: "उ", img: null, correct: false },
-      { text: "ए", img: null, correct: true }
+      { text: "आ", img: null, correct: true }
     ]
   },
   {
@@ -101,8 +101,8 @@ const quiz4 = [
     q: "'ढ' से संबंधित चित्र को पहचानिए:",
     img: null,
     opts: [
-      { text: "ढोलक", img: "images/p4_q11_dholak.png", correct: true },
-      { text: "छाता", img: "images/p4_q11_umbrella.png", correct: false }
+      { text: "ढोलक", img: "images/dholak.jpeg", correct: true },
+      { text: "टोकरी", img: "images/basket.jpeg", correct: false }
     ]
   },
 
@@ -121,8 +121,8 @@ const quiz4 = [
     q: "सही मिलान को पहचानिए:",
     img: null,
     opts: [
-      { text: "इमली - इ", img: "images/p4_q13_tamarind_i.png", correct: true },
-      { text: "अमरूद - ए", img: "images/p4_q13_guava_e.png", correct: false }
+      { text: "aam - इ", img: "images/mango.jpeg", correct: false },
+      { text: "एड़ी - ए", img: "images/heel.jpeg", correct: true }
     ]
   },
   {
@@ -130,8 +130,8 @@ const quiz4 = [
     q: "'अ' अक्षर वाले चित्र को चुनिए:",
     img: null,
     opts: [
-      { text: "अनार", img: "images/p4_q14_pomegranate.png", correct: true },
-      { text: "अमरूद", img: "images/p4_q14_guava.png", correct: false }
+      { text: "कमल", img: "images/lotus.jpeg", correct: false },
+      { text: "अमरूद", img: "images/guava.jpeg", correct: true }
     ]
   },
 
@@ -139,10 +139,10 @@ const quiz4 = [
   {
     id: 15,
     q: "चित्र के नाम का पहला अक्षर पहचानिए:",
-    img: "images/p4_q15_tamarind.png", // Tamarind (इमली)
+    img: "images/okhli.jpeg", // Tamarind (इमली)
     opts: [
       { text: "अ", img: null, correct: false },
-      { text: "इ", img: null, correct: true }
+      { text: "ओ", img: null, correct: true }
     ]
   },
   {
@@ -150,8 +150,8 @@ const quiz4 = [
     q: "'ए' से संबंधित चित्र को पहचानिए:",
     img: null,
     opts: [
-      { text: "एड़ी", img: "images/p4_q16_heel.png", correct: true },
-      { text: "उल्लू", img: "images/p4_q16_owl.png", correct: false }
+      { text: "एड़ी", img: "images/heel.jpeg", correct: true },
+      { text: "उल्लू", img: "images/owl.jpeg", correct: false }
     ]
   },
   {
@@ -159,15 +159,15 @@ const quiz4 = [
     q: "'इ' अक्षर वाले चित्र को चुनिए:",
     img: null,
     opts: [
-      { text: "उल्लू", img: "images/p4_q17_owl.png", correct: false },
-      { text: "इमली", img: "images/p4_q17_tamarind.png", correct: true }
+      { text: "इमली", img: "images/tamarind.jpeg", correct: false },
+      { text: "अंगूर", img: "images/green_grapes.jpeg", correct: true }
     ]
   },
 
   // Page 7 (Q18 to Q20)
   {
     id: 18,
-    q: "'ए' ....... 'ओ' के बीच में आने वाला अक्षर पहचानिए:",
+    q: "'ए' _ 'ओ' के बीच में आने वाला अक्षर पहचानिए:",
     img: null,
     opts: [
       { text: "आ", img: null, correct: false },
@@ -186,7 +186,7 @@ const quiz4 = [
   {
     id: 20,
     q: "चित्र से संबंधित अक्षर पहचानिए:",
-    img: "images/p4_q20_rishi.png", // Sage (ऋषि)
+    img: "images/rishi.jpeg", // Sage (ऋषि)
     opts: [
       { text: "आ", img: null, correct: false },
       { text: "ऋ", img: null, correct: true }
@@ -199,8 +199,8 @@ const quiz4 = [
     q: "'अं' अक्षर से संबंधित चित्र को पहचानिए:",
     img: null,
     opts: [
-      { text: "औरत", img: "images/p4_q21_woman.png", correct: false },
-      { text: "अंगूर", img: "images/p4_q21_grapes.png", correct: true }
+      { text: "औरत", img: "images/woman.jpeg", correct: false },
+      { text: "अंगूर", img: "images/green_grapes.jpeg", correct: true }
     ]
   },
   {
@@ -208,8 +208,8 @@ const quiz4 = [
     q: "'ई' अक्षर वाले चित्र को पहचानिए:",
     img: null,
     opts: [
-      { text: "ईख", img: "images/p4_q22_sugarcane.png", correct: true },
-      { text: "ओखली", img: "images/p4_q22_mortar.png", correct: false }
+      { text: "ईख", img: "images/sugarcane.jpeg", correct: true },
+      { text: "ओखली", img: "images/okhli.jpeg", correct: false }
     ]
   },
   {
@@ -217,8 +217,8 @@ const quiz4 = [
     q: "'ट' अक्षर वाला चित्र है:",
     img: null,
     opts: [
-      { text: "टमाटर", img: "images/p4_q23_tomato.png", correct: true },
-      { text: "उल्लू", img: "images/p4_q23_owl.png", correct: false }
+      { text: "टमाटर", img: "images/tomato.jpeg", correct: true },
+      { text: "अनार", img: "images/pomegranate.jpeg", correct: false }
     ]
   },
 
@@ -226,7 +226,7 @@ const quiz4 = [
   {
     id: 24,
     q: "जहाज और जग किस अक्षर से शुरू होते हैं?",
-    img: "images/p4_q24_ship_jug.png",
+    img: "images/ship.jpeg",
     opts: [
       { text: "ज", img: null, correct: true },
       { text: "क्ष", img: null, correct: false }
@@ -237,8 +237,8 @@ const quiz4 = [
     q: "सही मिलान को चुनिए:",
     img: null,
     opts: [
-      { text: "खरगोश - क", img: "images/p4_q25_rabbit_k.png", correct: false },
-      { text: "रथ - र", img: "images/p4_q25_chariot_r.png", correct: true }
+      { text: "खरगोश - क", img: "images/rabbit.jpeg", correct: false },
+      { text: "रथ - र", img: "images/chariot.jpeg", correct: true }
     ]
   },
 
@@ -248,23 +248,23 @@ const quiz4 = [
     q: "'ब' अक्षर से संबंधित चित्र को चुनिए:",
     img: null,
     opts: [
-      { text: "बत्तख", img: "images/p4_q26_duck.png", correct: true },
-      { text: "ठठेरा", img: "images/p4_q26_thathera.png", correct: false }
+      { text: "बत्तख", img: "duck.jpeg", correct: true },
+      { text: "ठठेरा", img: "images/thatera.jpeg", correct: false }
     ]
   },
   {
     id: 27,
     q: "समान चित्र को पहचानिए:",
-    img: "images/p4_q27_watermelon_ref.png", // Watermelon (तरबूज)
+    img: "images/watermelon.jpeg", // Watermelon (तरबूज)
     opts: [
-      { text: "तरबूज", img: "images/p4_q27_watermelon.png", correct: true },
-      { text: "अमरूद", img: "images/p4_q27_guava.png", correct: false }
+      { text: "तरबूज", img: "images/watermelon.jpeg", correct: true },
+      { text: "अमरूद", img: "images/guava.jpeg", correct: false }
     ]
   },
   {
     id: 28,
     q: "चित्र के नाम का पहला अक्षर है:",
-    img: "images/p4_q28_rabbit.png", // Rabbit (खरगोश)
+    img: "images/flower_pot.jpeg", // Flower pot
     opts: [
       { text: "ग", img: null, correct: false },
       { text: "ख", img: null, correct: true }
@@ -277,17 +277,17 @@ const quiz4 = [
     q: "'छ' अक्षर वाले चित्र को पहचानिए:",
     img: null,
     opts: [
-      { text: "छाता", img: "images/p4_q29_umbrella.png", correct: true },
-      { text: "मछली", img: "images/p4_q29_fish.png", correct: false }
+      { text: "छाता", img: "images/umbrella.png", correct: true },
+      { text: "ऐनक", img: "images/spectacles.png", correct: false }
     ]
   },
   {
     id: 30,
     q: "अलग चित्र को पहचानिए:",
-    img: null,
+    img: "images/fish.jpeg",
     opts: [
-      { text: "उल्लू", img: "images/p4_q30_owl.png", correct: true },
-      { text: "मछली", img: "images/p4_q30_fish.png", correct: false }
+      { text: "उल्लू", img: "images/owl.jpeg", correct: false },
+      { text: "मछली", img: "images/fish.jpeg", correct: true }
     ]
   },
   {
@@ -303,7 +303,7 @@ const quiz4 = [
   // Page 12 (Q32 to Q35)
   {
     id: 32,
-    q: "..... 'इ' खाली स्थान के लिए अक्षर चुनिए:",
+    q: "'अ' _ 'इ' खाली स्थान के लिए अक्षर चुनिए:",
     img: null,
     opts: [
       { text: "आ", img: null, correct: true },
@@ -313,7 +313,7 @@ const quiz4 = [
   {
     id: 33,
     q: "समान अक्षर को पहचानिए:",
-    img: null,
+    img: "images/gya_hindi_letter.jpeg",
     opts: [
       { text: "ण", img: null, correct: false },
       { text: "ज्ञ", img: null, correct: true }
@@ -322,15 +322,15 @@ const quiz4 = [
   {
     id: 34,
     q: "चित्र से संबंधित अक्षर को पहचानिए:",
-    img: "images/p4_q34_umbrella.png", // Umbrella (छाता)
+    img: "images/goat.jpeg", // Goat (बकरी)
     opts: [
       { text: "च", img: null, correct: false },
-      { text: "छ", img: null, correct: true }
+      { text: "ब", img: null, correct: true }
     ]
   },
   {
     id: 35,
-    q: "'आ' ..... 'ई' के बीच में आता है:",
+    q: "'आ' _ 'ई' के बीच में आता है:",
     img: null,
     opts: [
       { text: "अ", img: null, correct: false },
@@ -342,7 +342,7 @@ const quiz4 = [
   {
     id: 36,
     q: "चित्र से संबंधित अक्षर को पहचानिए:",
-    img: "images/p4_q36_kite.png", // Kite (पतंग)
+    img: "images/kite.jpeg", // Kite (पतंग)
     opts: [
       { text: "ख", img: null, correct: false },
       { text: "प", img: null, correct: true }
@@ -351,16 +351,16 @@ const quiz4 = [
   {
     id: 37,
     q: "दिए गए अक्षर से अलग अक्षर को पहचानो:",
-    img: null,
+    img: "images/jh_hindi_letter.jpeg",
     opts: [
       { text: "झ", img: null, correct: false },
-      { text: "ट", img: null, correct: true }
+      { text: "इ", img: null, correct: true }
     ]
   },
   {
     id: 38,
     q: "चित्र से संबंधित अक्षर को पहचानिए:",
-    img: "images/p4_q38_owl.png", // Owl (उल्लू)
+    img: "images/owl.jpeg", // Owl (उल्लू)
     opts: [
       { text: "अ", img: null, correct: false },
       { text: "उ", img: null, correct: true }
@@ -370,7 +370,7 @@ const quiz4 = [
   // Page 14 (Q39 to Q40)
   {
     id: 39,
-    q: "'ई' ..... 'ऊ' के बीच में आता है:",
+    q: "'ई' _ 'ऊ' के बीच में आता है:",
     img: null,
     opts: [
       { text: "उ", img: null, correct: true },
@@ -379,7 +379,7 @@ const quiz4 = [
   },
   {
     id: 40,
-    q: "च ....... ज खाली स्थान भरिए:",
+    q: "च _ ज खाली स्थान भरिए:",
     img: null,
     opts: [
       { text: "छ", img: null, correct: true },
